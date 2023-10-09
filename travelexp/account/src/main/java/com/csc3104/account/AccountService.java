93e4b67a535691ceead41df065ec0006c225fb98
huilunang <huilun2000@gmail.com>
2023-10-09 17:31:13 +0800

    Add base template

---
+++ travelexp/account/src/main/java/com/csc3104/account/AccountService.java
@@ -0,0 +1,5 @@
+package com.csc3104.account;
+
+public class AccountService {
+    // override gRPC generated code
+}
---
+++ travelexp/travel-management/src/main/java/com/csc3104/travelmanagement/TravelManagementApplication.java
@@ -0,0 +1,11 @@
+package com.csc3104.travelmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelManagementApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TravelManagementApplication.class, args);
+    }
+}
