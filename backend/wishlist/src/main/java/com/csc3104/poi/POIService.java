1b7840722ebe75a508f7645b5e7bc2e4be080535
ssihuiong <ongsihui01@gmail.com>
2023-11-02 18:06:57 +0800

    remove unused imports

--- backend/wishlist/src/main/java/com/csc3104/poi/POIService.java
+++ backend/wishlist/src/main/java/com/csc3104/poi/POIService.java
@@ -1,9 +1,7 @@
 package com.csc3104.poi;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.LinkedHashMap;
-import java.util.List;
 import java.util.Map;
 
 import org.json.JSONArray;
