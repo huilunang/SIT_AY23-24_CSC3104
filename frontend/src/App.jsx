5ba028a657b81c5e9d1eef4ddd796c90df063c88
huilunang <57522846+huilunang@users.noreply.github.com>
2023-11-06 23:49:13 +0800

    Add dockerised account service (#10)
    Co-authored-by: Natalie <joe44824@gmail.com>

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,18 +2,14 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import "./App.css";
 
 import LoginPage from "./pages/login";
-import HomePage from "./pages/home";
 import ErrorPage from "./pages/error";
-import LogoutPage from "./pages/logout";
 import RegisterPage from "./pages/register";
-import Gallery from "./pages/gallery";
+import GalleryPage from "./pages/gallery";
 import WishListPage from "./pages/wishlist";
 import POIPage from "./pages/poi";
 import RandomizerPage from "./pages/randomizer";
 
 import AuthProvider from "./security/AuthContext.jsx";
-
-import HeaderComponent from "./components/header";
 
 function AuthenticatedRoute({ children }) {
   if (localStorage.getItem("isAuthenticated") == "true") {
@@ -24,32 +20,65 @@ function AuthenticatedRoute({ children }) {
 }
 
 export default function App() {
+  const isAuthenticated = localStorage.getItem("isAuthenticated") === "true";
+
   return (
     <div className="TravelExp">
       <AuthProvider>
         <BrowserRouter>
-          <HeaderComponent />
           <Routes>
-            {/* Routes to bypass authentication here */}
-            <Route path="/gallery" element={<Gallery />}></Route>
-            <Route path="wishlist/:id" element={<WishListPage />}></Route>
-            <Route path="poi/:wishlistId/:businessId" element={<POIPage />}></Route>
-            <Route path="randomizer" element={<RandomizerPage />}></Route>
-            {/* Until here */}
-            <Route path="/" element={<LoginPage />}></Route>
-            <Route path="/login" element={<LoginPage />}></Route>
-            <Route path="/register" element={<RegisterPage />}></Route>
+            <Route
+              path="/"
+              element={
+                isAuthenticated ? <Navigate to="/gallery" /> : <LoginPage />
+              }
+            ></Route>
+            <Route
+              path="/login"
+              element={
+                isAuthenticated ? <Navigate to="/gallery" /> : <LoginPage />
+              }
+            ></Route>
+            <Route
+              path="/register"
+              element={
+                isAuthenticated ? <Navigate to="/gallery" /> : <RegisterPage />
+              }
+            ></Route>
 
             <Route
-              path="/home"
+              path="/gallery"
               element={
                 <AuthenticatedRoute>
-                  <HomePage />
+                  <GalleryPage />
+                </AuthenticatedRoute>
+              }
+            ></Route>
+            <Route
+              path="/wishlist/:id"
+              element={
+                <AuthenticatedRoute>
+                  <WishListPage />
+                </AuthenticatedRoute>
+              }
+            ></Route>
+            <Route
+              path="poi/:wishlistId/:businessId"
+              element={
+                <AuthenticatedRoute>
+                  <POIPage />
+                </AuthenticatedRoute>
+              }
+            ></Route>
+            <Route
+              path="/randomizer"
+              element={
+                <AuthenticatedRoute>
+                  <RandomizerPage />
                 </AuthenticatedRoute>
               }
             ></Route>
 
-            <Route path="/logout" element={<LogoutPage />}></Route>
             <Route path="*" element={<ErrorPage />}></Route>
           </Routes>
         </BrowserRouter>
