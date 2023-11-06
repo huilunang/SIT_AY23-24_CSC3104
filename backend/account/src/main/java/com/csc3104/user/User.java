c8b3c2b3fee775b3a8a20fe56444a86e10d4b0b9
Chloe Loh <54464018+ChloeLoh-Dsktop@users.noreply.github.com>
2023-11-07 00:13:22 +0800

    Update User.java
    Update to add friends list

--- backend/account/src/main/java/com/csc3104/user/User.java
+++ backend/account/src/main/java/com/csc3104/user/User.java
@@ -30,6 +30,8 @@ public class User implements UserDetails {
     private String email;
 
     private String password;
+    
+    private List<String> friends;
 
     @Enumerated(EnumType.STRING)
     private Role role;
