a4c5dd4010f200a644f5b57cb45269a915cfda6f
huilunang <huilun2000@gmail.com>
2023-11-07 00:27:00 +0800

    Add navbar

--- frontend/src/pages/events/index.jsx
+++ frontend/src/pages/events/index.jsx
@@ -1,12 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 import EventComponent from "./event-tab";
 
-import Card from 'react-bootstrap/Card';
-import { Row } from 'react-bootstrap';
-import { Col } from 'react-bootstrap';
+import Card from "react-bootstrap/Card";
+import { Row } from "react-bootstrap";
+import { Col } from "react-bootstrap";
 
 import { friendRequest } from "../../api/notification/NotificationApiService";
+import CustomNavbar from "../../components/navbar";
 
 const EventPage = () => {
   const [events, setEvents] = useState([]);
@@ -15,63 +16,73 @@ const EventPage = () => {
   // This is a test to send friend requests notification. (To remove later)
   async function friendRequestApi() {
     try {
-        const email = localStorage.getItem("email");
+      const email = localStorage.getItem("email");
 
-        await friendRequest("", email, "false", "friend-request", "requested")
-            .then((response) => successfulResponse(response))
-            .catch((error) => errorResponse(error))
-            .finally(() => console.log("cleanup"));
-        } catch (error) {
-          console.error("An error occurred:", error);
-      }
-  }  
+      await friendRequest("", email, "false", "friend-request", "requested")
+        .then((response) => successfulResponse(response))
+        .catch((error) => errorResponse(error))
+        .finally(() => console.log("cleanup"));
+    } catch (error) {
+      console.error("An error occurred:", error);
+    }
+  }
 
-  useEffect(() =>{
-  }, []);
+  useEffect(() => {}, []);
 
   return (
     <>
-    <EventComponent updateEvent={(event) => setEvents(event)} updateParty={(party) => setParties(party)}/>
-    {/* <div className="nav-link me-4" onClick={friendRequestApi}>
+      <CustomNavbar />
+      <EventComponent
+        updateEvent={(event) => setEvents(event)}
+        updateParty={(party) => setParties(party)}
+      />
+      {/* <div className="nav-link me-4" onClick={friendRequestApi}>
       click me
     </div> */}
-    {[...events].reverse().map((event, index) => (
-      <Card as={Row} key={index} style={{width: '100%'}}>
-        <Card.Body className="ps-5 pe-5">
-          <Row>
-            <Col sm="10">
-              <Card.Title style={{ fontSize: '36px' }}>{event.title}</Card.Title>
-              <Card.Text>
-            <>
-              Creator: {event.owner}<br />
-              Date: {event.description}<br />
-              Time: {event.time}<br />
-              Description: {event.description}<br />
-              Party: {event.invites}
-              <br />
-              Accepted:&nbsp;
-              {parties
-                .filter((party) => event.key === party.key && event.member == party.member)
-                .map((party) => (
-                  <span key={party.key}>
-                    {party.member}&nbsp;
-                  </span>
-                ))
-              } 
-              <br />
-              Notify: {event.notify}
-            </>
-          </Card.Text>
-            </Col>
-            <Col sm="2">
-              <Card.Text>
-                <Card.Img variant="top" src="holder.js/100px180" />
-              </Card.Text>
-            </Col>
-          </Row>
-        </Card.Body>
-      </Card>                
-    ))}
+      {[...events].reverse().map((event, index) => (
+        <Card as={Row} key={index} style={{ width: "100%" }}>
+          <Card.Body className="ps-5 pe-5">
+            <Row>
+              <Col sm="10">
+                <Card.Title style={{ fontSize: "36px" }}>
+                  {event.title}
+                </Card.Title>
+                <Card.Text>
+                  <>
+                    Creator: {event.owner}
+                    <br />
+                    Date: {event.description}
+                    <br />
+                    Time: {event.time}
+                    <br />
+                    Description: {event.description}
+                    <br />
+                    Party: {event.invites}
+                    <br />
+                    Accepted:&nbsp;
+                    {parties
+                      .filter(
+                        (party) =>
+                          event.key === party.key &&
+                          event.member == party.member
+                      )
+                      .map((party) => (
+                        <span key={party.key}>{party.member}&nbsp;</span>
+                      ))}
+                    <br />
+                    Notify: {event.notify}
+                  </>
+                </Card.Text>
+              </Col>
+              <Col sm="2">
+                <Card.Text>
+                  <Card.Img variant="top" src="holder.js/100px180" />
+                </Card.Text>
+              </Col>
+            </Row>
+          </Card.Body>
+        </Card>
+      ))}
     </>
   );
 };
