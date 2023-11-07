e42f83b8716ab340f49f689365bef39d96e59ab5
Natalie <joe44824@gmail.com>
2023-11-07 16:41:47 +0800

    added search user by email endpoint

---
+++ backend/account/src/main/java/com/csc3104/user/UserService.java
@@ -0,0 +1,35 @@
+package com.csc3104.user;
+
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public ResponseEntity<UserDTO> getUserByEmail(String email) {
+
+        var userOptional = userRepository.findByEmail(email);
+
+        if (userOptional.isPresent()) {
+            User user = userOptional.get();
+            UserDTO userDTO = new UserDTO();
+            userDTO.setFirstname(user.getFirstname());
+            userDTO.setLastname(user.getLastname());
+            userDTO.setEmail(user.getEmail());
+
+            return ResponseEntity.ok(userDTO);
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+}
