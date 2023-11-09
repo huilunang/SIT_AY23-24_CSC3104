8fef464b0aa012254af0a82e044ffd41f0527435
osshiya <osshiya@gmail.com>
2023-11-09 21:05:10 +0800

    fix attending and remove notification on event tab

--- frontend/src/pages/events/event-tab/index.jsx
+++ frontend/src/pages/events/event-tab/index.jsx
@@ -1,29 +1,21 @@
 import React, { useState, useEffect } from "react";
 import { Link, Outlet } from "react-router-dom";
 
-import { IoNotifications } from "react-icons/io5";
-import { IoAddCircle } from "react-icons/io5";
+import { MDBIcon } from "mdb-react-ui-kit";
 
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import Badge from "react-bootstrap/Badge";
 
 import { EventModal } from "../event-modal";
 
-import Notification from "../notification";
-
-import { readAllNotifications } from "../../../api/notification/NotificationApiService";
-
-import { getAllEvents, getUserName } from "../../../api/notification/EventApiService";
+import { getAllEvents } from "../../../api/notification/EventApiService";
 import { getAllParty } from "../../../api/notification/EventApiService";
+import { getUserName } from "../../../api/notification/EventApiService";
 
 const EventComponent = ({ updateEvent, updateParty }) => {
-  const [notificationCount, setNotificationCount] = useState(0); // Initialize the count to 0
-  const [isNotificationOpen, setNotificationOpen] = useState(false);
 
   const [isModalOpen, setModalOpen] = useState(false);
-  const parties = [];
 
   function successfulResponse(response) {
     // console.log("successful. here's data " + JSON.stringify(response.data));
@@ -54,15 +46,20 @@ const EventComponent = ({ updateEvent, updateParty }) => {
           item.ownername = user.firstname + " " + user.lastname;
 
           const invitees = item.invites.split(",").map((invite) => invite.trim());
-
-          const inviteePromises = invitees.map(async (invite) => {
-            const user = await getUser(invite);
-            return { email: invite, name: user.firstname + " " + user.lastname };
-          });
-
-          const inviteeData = await Promise.all(inviteePromises);
-
-          item.invitation = inviteeData;
+          if (invitees.length > 0) {
+            const inviteePromises = invitees.map(async (invite) => {
+              if (invite != ''){
+                const inviteUser = await getUser(invite);
+                return { email: invite, name: inviteUser.firstname + " " + inviteUser.lastname };
+              }else{
+                return '';                
+              }
+            });
+            const inviteeData = await Promise.all(inviteePromises);
+            item.invitation = inviteeData;
+          }else{
+            item.invitation = [];
+          }
 
           return item; // Return the updated content
         })
@@ -87,43 +84,27 @@ const EventComponent = ({ updateEvent, updateParty }) => {
 
   async function fetchParty(key) {
     try{
+      const response = await getAllParty(key);
+      const content = response.data;
 
-    const response = await getAllParty(key);
-    const content = response.data;
+      if (content.length > 0) {
+        const updatedContent = await Promise.all(
+          content.map(async (item) => {
+            const user = await getUser(item.member);
+            item.membername = user.firstname + " " + user.lastname;
+            return item;
+          })
+        );
 
-    const updatedContent = await Promise.all(
-      content.map(async (item) => {
-        const user = await getUser(item.member);
-        item.membername = user.firstname + " " + user.lastname;
-        return item;
-      })
-    );
-
-    return updatedContent;
+        return updatedContent;
+      } else {
+        // Handle the case where there are no party items
+        return [];
+      }
     } catch (error) {
       console.error("An error occurred:", error);
     }
   }
-
-  const openNotification = () => {
-    setNotificationOpen(true);
-    setNotificationCount(0);
-    readAllNotifications()
-      .then((response) => successfulResponse(response))
-      .catch((error) => errorResponse(error))
-      .finally(() => console.log("read"));
-    fetchEvent();
-  };
-
-  const closeNotification = () => {
-    setNotificationOpen(false);
-    setNotificationCount(0);
-    readAllNotifications()
-      .then((response) => successfulResponse(response))
-      .catch((error) => errorResponse(error))
-      .finally(() => console.log("read"));
-    fetchEvent();
-  };
 
   const openModal = () => {
     setModalOpen(true);
@@ -135,13 +116,20 @@ const EventComponent = ({ updateEvent, updateParty }) => {
     fetchEvent();
   };
 
+  const refreshEvents = async () => {
+    await fetchEvent();
+  };
+
   useEffect(() => {
     fetchEvent();
   }, []);
 
   return (
     <>
-      <Navbar expand="lg" className="bg-body-tertiary">
+      <Navbar
+        expand="lg"
+        className="bg-body-tertiary"
+      >
         <Container style={{ flexWrap: "nowrap" }}>
           <Navbar.Brand
             style={{ width: "50%", fontWeight: "bold", fontSize: "34px" }}
@@ -157,36 +145,16 @@ const EventComponent = ({ updateEvent, updateParty }) => {
               gap: "10px",
             }}
           >
-            <Link className="nav-link me-4" onClick={openModal}>
-              <IoAddCircle
-                style={{
-                  fontSize: "30px",
-                  fill: "#000000",
-                  fillOpacity: "54%",
-                }}
-              />
+            <Link className="nav-link me-4" onClick={refreshEvents}>
+              <MDBIcon fas icon="redo" style={{ fontSize: "24px" }} />{" "}
             </Link>
-            <Link className="nav-link" onClick={openNotification}>
-              <IoNotifications
-                style={{
-                  fontSize: "30px",
-                  fill: "#000000",
-                  fillOpacity: "54%",
-                }}
-              />{" "}
-              <Badge pill variant="primary">
-                {notificationCount}
-              </Badge>
+            <Link className="nav-link me-4" onClick={openModal}>
+              <MDBIcon fas icon="plus-circle" style={{ fontSize: "24px" }} />{" "}
             </Link>
           </Nav>
         </Container>
       </Navbar>
       <Outlet />
-      <Notification
-        isOpen={isNotificationOpen}
-        onClose={closeNotification}
-        updateNotificationCount={(count) => setNotificationCount(count)}
-      />
       <EventModal isOpen={isModalOpen} onClose={closeModal} />
     </>
   );
